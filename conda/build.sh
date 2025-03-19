098e51ed0c80ca6e44f1eb4352b14a3785aa5b19
Lupphes <dev@lupphes.com>
2025-03-19 08:37:38 +0100

    Let's try this

--- conda/build.sh
+++ conda/build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+# Install pip-based dependencies
+$PYTHON -m pip install . --no-deps --ignore-installed
+$PYTHON -m pip install vcfpy==0.13.8 dominate==2.9.1
+
+# Create binary and make it executable
 mkdir -p $PREFIX/bin
-cp $SRC_DIR/bin/varify.py $PREFIX/bin/varify.py
-chmod +x $PREFIX/bin/varify.py
-
-
-# Install pip-only dependencies
-$PYTHON -m pip install --no-cache-dir vcfpy==0.13.8 dominate==2.9.1
+cp $SRC_DIR/bin/varify.py $PREFIX/bin/varify
+chmod +x $PREFIX/bin/varify
