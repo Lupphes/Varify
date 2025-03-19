1ee618294604367b5dbf753e57d57a4b18890c3e
Lupphes <dev@lupphes.com>
2025-03-19 11:38:01 +0100

    test

--- conda/build.sh
+++ conda/build.sh
@@ -8,6 +8,3 @@ cp -r $SRC_DIR/bin/varify $PREFIX/bin/
 
 # Make sure the main.py file is executable
 chmod +x $PREFIX/bin/varify/main.py
-
-# Create a symlink so 'varify' points to 'main.py'
-ln -s $PREFIX/bin/varify/main.py $PREFIX/bin/varify
