8176dc28592fcc18471acc43974c844d8ff4f9a2
Lupphes <dev@lupphes.com>
2025-03-19 07:28:47 +0100

    Hopefully publish

--- conda/build.sh
+++ conda/build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 mkdir -p $PREFIX/bin
-cp bin/varify.py $PREFIX/bin/varify
-chmod +x $PREFIX/bin/varify
+cp $SRC_DIR/bin/varify.py $PREFIX/bin/varify.py
+chmod +x $PREFIX/bin/varify.py
+
 
 # Install pip-only dependencies
 $PYTHON -m pip install --no-cache-dir vcfpy==0.13.8 dominate==2.9.1
